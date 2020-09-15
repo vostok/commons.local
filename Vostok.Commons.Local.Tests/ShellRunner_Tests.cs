338f2b2e8df6bac42f40e7934afb94983799cbca
kungurtsev <kungurtsev@skbkontur.ru>
2020-09-15 19:02:28 +0500

    + tests

---
+++ Vostok.Commons.Local.Tests/ShellRunner_Tests.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading;
+using FluentAssertions;
+using NUnit.Framework;
+using Vostok.Commons.Time;
+using Vostok.Logging.Console;
+
+namespace Vostok.Commons.Local.Tests
+{
+    [TestFixture]
+    internal class ShellRunner_Tests
+    {
+        [Test]
+        public void Start_Stop_should_work()
+        {
+            var runner = new ShellRunner(
+                new ShellRunnerSettings("ping")
+                {
+                    Arguments = "google.com -n 100"
+                },
+                new SynchronousConsoleLog());
+
+            runner.Start();
+
+            Thread.Sleep(5.Seconds());
+
+            runner.Stop();
+
+            Thread.Sleep(5.Seconds());
+        }
+
+        [Test]
+        public void Run_should_work()
+        {
+            var runner = new ShellRunner(
+                new ShellRunnerSettings("ping")
+                {
+                    Arguments = "google.com -n 2"
+                },
+                new SynchronousConsoleLog());
+
+            runner.RunAsync(5.Seconds(), CancellationToken.None)
+                .Wait(5.Seconds()).Should().BeTrue();
+        }
+
+        [Test]
+        public void Run_should_stop_after_timeout()
+        {
+            var runner = new ShellRunner(
+                new ShellRunnerSettings("ping")
+                {
+                    Arguments = "google.com -n 100"
+                },
+                new SynchronousConsoleLog());
+
+            runner.RunAsync(5.Seconds(), CancellationToken.None)
+                .ContinueWith(_ => {})
+                .Wait(10.Seconds()).Should().BeTrue();
+        }
+    }
+}
